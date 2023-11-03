4762310425e748115e8081214cf20f0e478c420b
dwaynediddy <burrows.dwayne@icloud.com>
2023-11-03 15:15:55 +0100

    added forms with input fields

--- src/Handler/Welcome.hs
+++ src/Handler/Welcome.hs
@@ -5,6 +5,7 @@
 
 module Handler.Welcome where
 
+import Data.Text (Text)
 import Yesod
 
 data Links = Links
@@ -21,32 +22,40 @@ instance Yesod Links
 
 getSignUpR :: Handler Html
 getSignUpR = defaultLayout $ do
-  setTitle("Sign Up")
+  setTitle ("Sign Up")
   [whamlet|
+  <form method=post action=@{LoginR}>
+  <div>
+    <label for="username">Add A Username:
+    <input type="text" id="username" name="username" required>
+    <label for="password">enter a password:
+    <input type="password" id="password" name="password" required>
+    <input type=submit value="sign up">
     <div>
       <button>
-        <a href=@{LoginR}>gpo to login
-
+        <a href=@{LoginR}>go to login
     <div>
       <p>already have an accoutn? log in ehre
   |]
 
 getLoginR :: Handler Html
 getLoginR = defaultLayout $ do
-  setTitle("Log in")
+  setTitle ("Log in")
   [whamlet|
-  <h2>Login Here
-  <div>
-    <p>an input for username/email will go here
-    <p> input for password
-  <div>
-    <p>dont have an account sign up here
-      <button><a href=@{SignUpR}>sign up
-  |]
+    <h2>Login Here
+    <form method=post action=@{LoginR}>
+      <div>
+        <label for="username">Username:
+        <input type="text" id="username" name="username" required>
+        <input type=submit value="Log in">
+    <div>
+      <p>dont have an account sign up here
+        <button><a href=@{SignUpR}>sign up
+    |]
 
 getWelcomeR :: Handler Html
 getWelcomeR = defaultLayout $ do
-  setTitle("Welcome")
+  setTitle ("Welcome")
   [whamlet|
   <h2>welcome to my page
   <div>
@@ -55,7 +64,7 @@ getWelcomeR = defaultLayout $ do
   <div>
     <p> this is the login page
       <button>
-        <a href=@{LoginR}>gpo to login
+        <a href=@{LoginR}>go to login
   <div>
   
 |]
