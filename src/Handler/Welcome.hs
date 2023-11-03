f7ecfdf5c1ca83868214401f77f3bac7a0fdf486
dwaynediddy <burrows.dwayne@icloud.com>
2023-11-03 12:30:16 +0100

    basic routing to login/sign up pages

--- src/Handler/Welcome.hs
+++ src/Handler/Welcome.hs
@@ -19,8 +19,45 @@ mkYesod
 
 instance Yesod Links
 
-getSignUpR = defaultLayout [whamlet|<a href=@{SignUpR}>go to sign up|]
-getLoginR = defaultLayout [whamlet|<a href=@{LoginR}>gpo to login|]
-getWelcomeR = defaultLayout [whamlet|<a href=@{WelcomeR}>go to welcome|]
+getSignUpR :: Handler Html
+getSignUpR = defaultLayout $ do
+  setTitle("Sign Up")
+  [whamlet|
+    <div>
+      <button>
+        <a href=@{LoginR}>gpo to login
+
+    <div>
+      <p>already have an accoutn? log in ehre
+  |]
+
+getLoginR :: Handler Html
+getLoginR = defaultLayout $ do
+  setTitle("Log in")
+  [whamlet|
+  <h2>Login Here
+  <div>
+    <p>an input for username/email will go here
+    <p> input for password
+  <div>
+    <p>dont have an account sign up here
+      <button><a href=@{SignUpR}>sign up
+  |]
+
+getWelcomeR :: Handler Html
+getWelcomeR = defaultLayout $ do
+  setTitle("Welcome")
+  [whamlet|
+  <h2>welcome to my page
+  <div>
+    <button>
+      <a href=@{SignUpR}>sign up here
+  <div>
+    <p> this is the login page
+      <button>
+        <a href=@{LoginR}>gpo to login
+  <div>
+  
+|]
 
 main = warp 3000 Links
