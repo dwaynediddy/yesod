8059aeb84ba9439c3bb6e66e13348e11963d7eaf
dwaynediddy <burrows.dwayne@icloud.com>
2023-11-06 15:24:55 +0100

    trying to add static image to welcome.hs HomeR

--- src/Handler/Welcome.hs
+++ src/Handler/Welcome.hs
@@ -5,25 +5,28 @@
 
 module Handler.Welcome where
 
+import Control.Applicative ((<$>), (<*>))
 import Data.Text (Text)
 import Yesod
 
-data Links = Links
+data App = App
 
 mkYesod
-  "Links"
+  "App"
   [parseRoutes|
 / WelcomeR GET
 /signup SignUpR GET
 /login LoginR GET
+/home HomeR GET
 |]
 
-instance Yesod Links
+instance Yesod App
 
 getSignUpR :: Handler Html
 getSignUpR = defaultLayout $ do
   setTitle ("Sign Up")
   [whamlet|
+  <h2> Sign up
   <form method=post action=@{LoginR}>
   <div>
     <label for="username">Add A Username:
@@ -31,11 +34,9 @@ getSignUpR = defaultLayout $ do
     <label for="password">enter a password:
     <input type="password" id="password" name="password" required>
     <input type=submit value="sign up">
-    <div>
-      <button>
-        <a href=@{LoginR}>go to login
-    <div>
-      <p>already have an accoutn? log in ehre
+  <div>
+    <p>already have an account? log in here
+      <button><a href=@{LoginR}>go to login
   |]
 
 getLoginR :: Handler Html
@@ -47,10 +48,12 @@ getLoginR = defaultLayout $ do
       <div>
         <label for="username">Username:
         <input type="text" id="username" name="username" required>
+        <label for="password">Password:
+        <input type="text" id="password" name="password" required>
         <input type=submit value="Log in">
-    <div>
-      <p>dont have an account sign up here
-        <button><a href=@{SignUpR}>sign up
+      <div>
+        <p>dont have an account sign up here
+          <button><a href=@{SignUpR}>sign up
     |]
 
 getWelcomeR :: Handler Html
@@ -69,4 +72,26 @@ getWelcomeR = defaultLayout $ do
   
 |]
 
-main = warp 3000 Links
+getHomeR :: Handler Html
+getHomeR = defaultLayout $ do
+  setTitle ("Home page")
+
+[whamlet|
+    <nav>
+        <ul>
+            <li>
+                <a href=@{HomeR}>Home
+            <li>
+                <a href=@{HomeR}>About
+            <li>
+                <a href=@{HomeR}>Products
+            <li>
+                <a href=@{HomeR}>Contacts
+    <h2>Welcome to my page
+    <div>
+        <h3>Main Section
+            <p>Look at the cute animals
+            <img src=@{StaticR kot.png} alt="cute animal">
+|]
+
+main = warp 3000 App
